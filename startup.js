bf9d5150229c1e756dc6f6701201a0522b3b8672
davidw96 <davidw96@users.noreply.github.com>
2020-05-08 19:20:35 +0200

    reformed code

--- startup.js
+++ startup.js
@@ -125,11 +125,11 @@ localizer.initializeNLSFragments(nls);
 
 var config = null;
 var configArg = getParameterByName("config");
-var configFile = 'configs/Decoder_test.yaml';
+var configFile = 'configs/reform_test.yaml';
 if (configArg) {
     configFile = 'configs/' + configArg;
 } else {
-    configFile = 'configs/Decoder_test.yaml';
+    configFile = 'configs/reform_test.yaml';
   }
 
   var pageManager = null;
