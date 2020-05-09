bd1f84fc0216c102c4ab472187eb3a77e7abacac
davidw96 <davidw96@users.noreply.github.com>
2020-05-09 18:27:32 +0200

    Update startup.js

--- startup.js
+++ startup.js
@@ -125,11 +125,11 @@ localizer.initializeNLSFragments(nls);
 
 var config = null;
 var configArg = getParameterByName("config");
-var configFile = 'configs/Decoder_test.yaml';
+var configFile = 'configs/full_test.yaml';
 if (configArg) {
     configFile = 'configs/' + configArg;
 } else {
-    configFile = 'configs/Decoder_test.yaml';
+    configFile = 'configs/full_test.yaml';
   }
 
   var pageManager = null;
